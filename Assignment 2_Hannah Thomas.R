613d3abc0dee897bd2f9cef76d4d29cb7a1fa1f1
Hannah Thomas <hannahthomas@live.ca>
2025-04-22 15:58:58 -0700

    moved summary to README.me

--- Assignment 2_Hannah Thomas.R
+++ Assignment 2_Hannah Thomas.R
@@ -67,14 +67,4 @@ ggplot(CVrisk_data, aes(x = age, y = cost, color=female)) +
        color= "Sex") +
   theme_minimal()
 
-#Summary of findings
-#From this sample of 5000 individuals hospitalized to a cardiac ward, inpatient costs associated with female patients were significantly less (by about 250 USD)
-#compared to male patients when adjusting for patient age, smoking status and presence of cardiac event.
-#From Figure 1, you can see that even as patients age, costs for female patients remain lower compared to male patients.
-#Potential explanations include that female patients received fewer inpatient investigations and cost-incurring care.
-#Another possible explanation is that females had a much lower rate of cardiac events than males (12% vs. 88% in Table 1). By virtue of this,
-#they likely underwent fewer cost-incurring cardiac procedures compared to males, although we would need additional data to confirm.
-#Overall, at the individual-level 250 USD may not seem like a meaningful difference; however, this can certainly compound at the population-level.
-#Additional research investigating the mechanism behind lower costs for female patients is warranted.
-
 #I did not use generative AI technology to complete any portion of the work.
